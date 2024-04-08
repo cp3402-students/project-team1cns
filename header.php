4f2a7d0b2c77d3b4b86bf0ec92fb80b01a3b8ce2
daynemorris <daynemorris14@gmail.com>
2024-04-08 16:26:43 +1000

    wrap site title and image into a div so it can be flexed

--- header.php
+++ header.php
@@ -27,10 +27,12 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
+			<?php the_custom_logo(); ?>
+
+			<div class="site-branding_text">
+
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
+			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
@@ -43,6 +45,7 @@
 				?>
 				<p class="site-description"><?php echo $theme_for_u3a_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
+			</div> <!-- .site-branding_text -->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
