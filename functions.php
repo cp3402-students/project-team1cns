275ccb3e4df5bfee6df16b87806a6b30793d5356
daynemorris <daynemorris14@gmail.com>
2024-04-10 00:37:59 +1000

    change register_nav_menu

--- functions.php
+++ functions.php
@@ -49,7 +49,7 @@ function theme_for_u3a_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'theme-for-u3a' ),
+			'menu-1' => esc_html__( 'Header', 'theme-for-u3a' ),
 		)
 	);
 
