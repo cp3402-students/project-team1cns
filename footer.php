8ceebae38b169216eacdc2a92795aeda23f218a9
trijeetbhandula <trijeet.bhandula@my.jcu.edu.au>
2024-04-12 15:47:44 +1000

    Footer Updates

--- footer.php
+++ footer.php
@@ -12,16 +12,21 @@
 ?>
 
     <footer id="colophon" class="site-footer">
-        
-        <!-- Widget Areas -->
-        <div class="footer-widgets">
-            <div class="widget-area">
-                <?php if (is_active_sidebar('footer-1')) : ?>
-                    <?php dynamic_sidebar('footer-1'); ?>
-                <?php endif; ?>
-            </div>
-        </div><!-- .footer-widgets -->
-        
+  
+
+        <div class="site-info">
+            <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'theme-for-u3a' ) ); ?>">
+                <?php
+                /* translators: %s: CMS name, i.e. WordPress. */
+                printf( esc_html__( 'Proudly powered by %s', 'theme-for-u3a' ), 'WordPress' );
+                ?>
+            </a>
+            <span class="sep"> | </span>
+                <?php
+                /* translators: 1: Theme name, 2: Theme author. */
+                printf( esc_html__( 'Theme: %1$s by %2$s.', 'theme-for-u3a' ), 'theme-for-u3a', '<a href="http://underscores.me/">Underscores.me</a>' );
+                ?>
+        </div><!-- .site-info -->
     </footer><!-- #colophon -->
 </div><!-- #page -->
 
